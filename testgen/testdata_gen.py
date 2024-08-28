1d3d83737f1af75eb15e64943f91dfdefd1f04df
Matt Radbourne <1254508+mradbourne@users.noreply.github.com>
2024-08-28 18:29:47 +0100

    Remove main function and fix paths

--- testgen/testdata_gen.py
+++ testgen/testdata_gen.py
@@ -1,34 +1,21 @@
 # -*- coding: utf-8 -*-
-import argparse
 import logging
 import logging.config
 import multiprocessing as mp
 import re
 
-from test_type import TestType, test_types
-from generators.collation_short import CollationShortGenerator
-from generators.datetime_fmt import DateTimeFmtGenerator
-from generators.lang_names import LangNamesGenerator
-from generators.likely_subtags import LikelySubtagsGenerator
-from generators.message_fmt2 import MessageFmt2Generator
-from generators.list_fmt import ListFmtGenerator
-from generators.number_fmt import NumberFmtGenerator
-from generators.plurals import PluralGenerator
-from generators.relativedatetime_fmt import RelativeDateTimeFmtGenerator
+from conformance_cli.test_type import TestType
+from .generators.collation_short import CollationShortGenerator
+from .generators.datetime_fmt import DateTimeFmtGenerator
+from .generators.lang_names import LangNamesGenerator
+from .generators.likely_subtags import LikelySubtagsGenerator
+from .generators.message_fmt2 import MessageFmt2Generator
+from .generators.list_fmt import ListFmtGenerator
+from .generators.number_fmt import NumberFmtGenerator
+from .generators.plurals import PluralGenerator
+from .generators.relativedatetime_fmt import RelativeDateTimeFmtGenerator
 
 reblankline = re.compile("^\s*$")
-
-
-def setupArgs():
-    parser = argparse.ArgumentParser(prog="testdata_gen")
-    parser.add_argument("--icu_versions", nargs="*", default=[])
-    parser.add_argument(
-        "--test_types", nargs="*", choices=test_types, default=test_types
-    )
-    # -1 is no limit
-    parser.add_argument("--run_limit", nargs="?", type=int, default=-1)
-    new_args = parser.parse_args()
-    return new_args
 
 
 def generate_versioned_data_parallel(args):
@@ -64,58 +51,53 @@ def generate_versioned_data(version_info):
 
     if TestType.COLLATION_SHORT in args.test_types:
         # This is slow
-        generator = CollationShortGenerator(icu_version, args.run_limit)
+        generator = CollationShortGenerator(icu_version, args.testgen_run_limit)
         generator.process_test_data()
 
-    ## DISABLED FOR NOW. Use new .js generator with Node version
+    # DISABLED FOR NOW. Use new .js generator with Node version
     if TestType.DATETIME_FMT in args.test_types:
         # This is slow
-        generator = DateTimeFmtGenerator(icu_version, args.run_limit)
+        generator = DateTimeFmtGenerator(icu_version, args.testgen_run_limit)
         generator.process_test_data()
 
     if TestType.LIST_FMT in args.test_types:
         # This is slow
-        generator = ListFmtGenerator(icu_version, args.run_limit)
+        generator = ListFmtGenerator(icu_version, args.testgen_run_limit)
         generator.process_test_data()
 
     if TestType.LANG_NAMES in args.test_types:
         # This is slow
-        generator = LangNamesGenerator(icu_version, args.run_limit)
+        generator = LangNamesGenerator(icu_version, args.testgen_run_limit)
         generator.process_test_data()
 
     if TestType.LIKELY_SUBTAGS in args.test_types:
-        generator = LikelySubtagsGenerator(icu_version, args.run_limit)
+        generator = LikelySubtagsGenerator(icu_version, args.testgen_run_limit)
         generator.process_test_data()
 
     if TestType.MESSAGE_FMT2 in args.test_types:
-        generator = MessageFmt2Generator(icu_version, args.run_limit)
+        generator = MessageFmt2Generator(icu_version, args.testgen_run_limit)
         generator.process_test_data()
 
     if TestType.NUMBER_FMT in args.test_types:
-        generator = NumberFmtGenerator(icu_version, args.run_limit)
+        generator = NumberFmtGenerator(icu_version, args.testgen_run_limit)
         generator.process_test_data()
 
     if TestType.RELATIVE_DATETIME_FMT in args.test_types:
         # This is slow
-        generator = RelativeDateTimeFmtGenerator(icu_version, args.run_limit)
+        generator = RelativeDateTimeFmtGenerator(icu_version, args.testgen_run_limit)
         generator.process_test_data()
 
     if TestType.PLURAL_RULES in args.test_types:
         # This is slow
-        generator = PluralGenerator(icu_version, args.run_limit)
+        generator = PluralGenerator(icu_version, args.testgen_run_limit)
         generator.process_test_data()
     logging.info("++++ Data generation for %s is complete.", icu_version)
 
 
-def main():
-    new_args = setupArgs()
-
+def run(args):
+    logging.config.fileConfig("logging.conf")
     logger = logging.Logger("TEST_GENERATE LOGGER")
     logger.setLevel(logging.INFO)
 
     # Generate version data in parallel if possible
-    generate_versioned_data_parallel(new_args)
-
-
-if __name__ == "__main__":
-    main()
+    generate_versioned_data_parallel(args)
